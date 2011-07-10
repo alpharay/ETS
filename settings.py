db4277056f20b8ee9c7b56edc860c2343e6962eb
maka <makacodewalker@makacodewalker-laptop.(none)>
2011-07-10 21:56:57 +0000

    added template dirs

--- settings.py
+++ settings.py
@@ -1,7 +1,9 @@
 # Django settings for ETS project.
 
 import os # put this at the top of the file
+
 PROJECT_ROOT = os.path.realpath(os.path.dirname(__file__))
+DIRNAME = os.path.abspath(os.path.dirname(__file__))
 
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
@@ -48,12 +50,12 @@ USE_L10N = True
 
 # Absolute filesystem path to the directory that will hold user-uploaded files.
 # Example: "/home/media/media.lawrence.com/media/"
-MEDIA_ROOT = ''
+MEDIA_ROOT = ''#os.path.join(DIRNAME, 'static/')
 
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash.
 # Examples: "http://media.lawrence.com/media/", "http://example.com/media/"
-MEDIA_URL = ''
+MEDIA_URL = ''#/static/'
 
 # Absolute path to the directory static files should be collected to.
 # Don't put anything in this directory yourself; store your static files
@@ -122,7 +124,7 @@ INSTALLED_APPS = (
     'baseApp',
     'reg',
     # Uncomment the next line to enable the admin:
-     'django.contrib.admin',
+    'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
 )
@@ -149,3 +151,10 @@ LOGGING = {
         },
     }
 }
+
+#if settings.DEBUG:
+#    urlpatterns += patterns('django.views.static',                            
+#                            (r'^%s(?P<path>.*)$' % (settings.MEDIA_URL[1:],),'serve', 
+#                             {'document_root': settings.MEDIA_ROOT,'show_indexes': True }),)
+
+
